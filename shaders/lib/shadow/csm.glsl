b977c2bd10e550e962cd8b376ff1ba70a7237185
NULL511 <null511@outlook.com>
2024-12-16 01:11:26 -0500

    shadow filter padding

--- shaders/lib/shadow/csm.glsl
+++ shaders/lib/shadow/csm.glsl
@@ -1,9 +1,12 @@
 void GetShadowProjection(in vec3 shadowViewPos, out int cascadeIndex, out vec3 shadowPos) {
+    float shadowPixel = 1.0 / shadowMapResolution;
+    float padding = 4.0 * shadowPixel;
+
     for (int i = 0; i < 4; i++) {
         shadowPos = (shadowProjection[i] * vec4(shadowViewPos, 1.0)).xyz;
         cascadeIndex = i;
 
-        if (clamp(shadowPos, -1.0, 1.0) == shadowPos) break;
+        if (clamp(shadowPos, -1.0 + padding, 1.0 - padding) == shadowPos) break;
     }
 }
 

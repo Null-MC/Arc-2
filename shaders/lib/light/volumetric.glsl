c479d3b99cadc46163ea6d36451cba450e9e5bf0
NULL511 <null511@outlook.com>
2025-05-03 03:38:28 -0400

    water color fixes

--- shaders/lib/light/volumetric.glsl
+++ shaders/lib/light/volumetric.glsl
@@ -11,6 +11,6 @@ const float VL_WaterPhaseF =  0.86;
 const float VL_WaterPhaseB = -0.14;
 const float VL_WaterPhaseM =  0.65;
 
-vec3 VL_WaterScatter = 0.0002 * RgbToLinear(vec3(0.545, 0.682, 0.690));
+vec3 VL_WaterScatter = 0.004 * RgbToLinear(vec3(0.545, 0.682, 0.690));
 vec3 VL_WaterTransmit = RgbToLinear(1.0 - vec3(0.318, 0.576, 0.769));
-vec3 VL_WaterAmbient = 1000.0*RgbToLinear(vec3(0.325, 0.588, 0.439));
+vec3 VL_WaterAmbient = 1.0*RgbToLinear(vec3(0.325, 0.588, 0.439));
